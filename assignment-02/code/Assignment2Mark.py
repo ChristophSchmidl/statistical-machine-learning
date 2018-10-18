0a1ceab54d2bd3f2753af9dbfa6e77aae3fa3fbb
Mark Beijer <mbeijer@science.ru.nl>
2018-10-18 13:28:32 +0200

    Added code directory and code, did some work on Exercise 2

---
+++ assignment-02/code/Assignment2Mark.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 18 12:23:45 2018
+
+@author: mbeijer
+"""
+
+"""
+Assignment 2 - Exercise 2
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%% Constants
+
+
+Alpha = 90
+Beta = 110
+
+#%% Functions
+
+
+def BetaDis(Mu,Alpha,Beta):
+    Frac = np.math.Gamma
