604783227b578dd1ce73ca3de58f7a740c96aa6f
Mark Beijer <mbeijer@science.ru.nl>
2018-11-13 17:16:11 +0100

    Made a plot for exercise 3

--- assignment-03/code/Exercise3.py
+++ assignment-03/code/Exercise3.py
@@ -18,16 +18,10 @@ y2 = np.abs(1-2*x)
 plt.plot(x,y1,label='$\lambda_1$')
 plt.plot(x,y2,label='$\lambda_2$')
 
-plt.xlabel('$\eta$',font_size=40)
-plt.ylabel('$r_n$' )
-
-plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=40)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+plt.xlabel('$\eta$',size=40)
+plt.ylabel('$r_n$',size=40)
 
 
-plt.legend()
+
+
+plt.legend(size=50)
