a60a125ffd5968ed4eea760d5ce1bd84053e5225
Mark Beijer <mbeijer@science.ru.nl>
2019-01-09 16:10:34 +0100

    Did some last things at ex 2

--- assignment-04/code/Exercise2.py
+++ assignment-04/code/Exercise2.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from time import time
 from mpl_toolkits.mplot3d import Axes3D
-
+import os
 
 import matplotlib.pylab as pylab
 params = {'legend.fontsize': 'x-large',
@@ -40,6 +40,7 @@ def Plot(X,Y,Name,save=False,savename=None):
     ax.set_zlabel('\nY  ',fontsize=50)
     ax.set_ylabel('\n$X_1$  ',fontsize=50)
     ax.set_xlabel('\n\n$X_0$',fontsize=50)
+    
     if(save):
         pylab.get_current_fig_manager().window.showMaximized()
         ax.set_xlim3d([-2, 2])
@@ -47,16 +48,19 @@ def Plot(X,Y,Name,save=False,savename=None):
         ax.set_zlim3d([0, 1.5])
         plt.savefig(savename,dpi=100,layout='tight_layout')
         plt.close(Name)
+        return
+    plt.show()
 
 
 
 class NeuralNW():
-    def __init__(self,X,Y,eta):
+    def __init__(self,X,Y,eta=0.1,D=2,K=1,M=8):
         self.w1 = np.random.random_sample((D+1,M))-0.5
         self.w2 = np.random.random_sample(M+1)-0.5
         self.X = X
         self.Y = Y
         self.TrainX = []
+        self.eta = eta
         for x in self.X:
             self.TrainX.append(np.array([1,x[0],x[1]]))
         self.TrainX = np.array(self.TrainX)
@@ -74,13 +78,13 @@ class NeuralNW():
 
     def TrainNetwork(self,Num):
         self.setNetworkOnce(Num)
-        Delta2 = self.Output - Y[Num]
+        Delta2 = self.Output - self.Y[Num]
         Delta1 = (1-np.square(self.HiddenLayer))*self.w2*Delta2
         Der2 = Delta2*self.HiddenLayer
         Delta1 = Delta1[1:]
         Der1 = np.array([Delta1*self.TrainX[Num][0],Delta1*self.TrainX[Num][1] , Delta1*self.TrainX[Num][2]])
-        self.w2 -= eta*Der2
-        self.w1 -= eta*Der1
+        self.w2 -= self.eta*Der2
+        self.w1 -= self.eta*Der1
         
     def TrainNetworkAll(self):
         for i in range(len(self.X)):
@@ -92,7 +96,7 @@ class NeuralNW():
     
     def Distance(self):
         Out = []
-        for i in range(len(X)):
+        for i in range(len(self.X)):
             self.setNetworkOnce(i)
             Out.append(self.Output)
         Out = np.array(Out)
@@ -109,12 +113,14 @@ class NeuralNW():
             
     def PlotOutput(self,Name,save=False,savename=None):
         Out = []
-        for i in range(len(X)):
+        for i in range(len(self.X)):
             self.setNetworkOnce(i)
             Out.append(self.Output)
         Out = np.array(Out)
         Plot(self.X,Out,Name,save=save,savename=savename)
-
+#%%
+import sys
+sys.exit("Error message")
 
 
 #%%Constants
@@ -130,6 +136,8 @@ for i in range(len(x)):
 X = np.array(X)
 Y = np.array([Gaus(x) for x in X])
 
+#%%2.1
+
 Plot(X,Y,"Plot Gaussian")
 
 
@@ -137,42 +145,37 @@ Plot(X,Y,"Plot Gaussian")
 
 
 
-#%%
-"""
-eta = 0.1
-D = 2
-M = 8
-Network = NeuralNW(X,Y,eta)
+#%% 2.2 + 2.3
+
+
+Network = NeuralNW(X,Y)
 Now = time()
 
-Network.PlotOutput("First")
+Network.PlotOutput("Initial_Output")
 
 for i in range(2000):
     if (i == 199):
-        Network.PlotOutput("Second")
-        print(time()-Now)
+        Network.PlotOutput("Plot_200_iterations")
     Network.TrainNetworkAll()
 
-Network.PlotOutput("Third")
-print(time()-Now)
-"""
+Network.PlotOutput("Plot_2000_iterations")
+
 #%%
-eta = 0.1
-D = 2
-M = 8
 
-
-NetworkRand = NeuralNW(X,Y,eta)
-for i in range(100):
+NetworkRand = NeuralNW(X,Y)
+for i in range(20):
     NetworkRand.TrainNetworkRand()
-print(NetworkRand.Distance())
+print("Error for the random training = ",NetworkRand.Distance())
 
-NetworkRand.PlotOutput("Test")
+NetworkRand.PlotOutput("Random_Training")
 
-NetworkNorm = NeuralNW(X,Y,eta)
+NetworkNorm = NeuralNW(X,Y)
 for i in range(20):
     NetworkNorm.TrainNetworkAll()
-print(NetworkNorm.Distance())
+print("Error for the sequential network = ",NetworkNorm.Distance())
+
+NetworkNorm.PlotOutput("Sequential_Training")
+
 
 #%% Testing - Amount of Nodes
 
@@ -183,7 +186,7 @@ Mlist = np.linspace(1,50,50,dtype=np.int)
 
 for M in Mlist:
     print(M)
-    NetworkTestM = NeuralNW(X,Y,eta)
+    NetworkTestM = NeuralNW(X,Y,M=M)
     now = time()
     for i in range(100):
         NetworkTestM.TrainNetworkRand()
@@ -201,14 +204,13 @@ plt.xlabel('M')
 plt.ylabel('t(s)')
 #%% Testing - Eta 
 Errorseta = []
-M = 8
 EtaLog = np.linspace(-4,2,50)
 
 Etas = np.power(10,EtaLog)
 
 for eta in Etas:
     print(eta)
-    NetworkTesteta = NeuralNW(X,Y,eta)
+    NetworkTesteta = NeuralNW(X,Y,eta=eta)
     for i in range(100):
         NetworkTesteta.TrainNetworkRand()
     Errorseta.append(NetworkTesteta.Distance())
@@ -221,9 +223,9 @@ plt.ylabel('Error')
 #%% Error Weights
 ErrorW = []
 Weights = []
-eta = 0.1
+D = 2
+M = 8
 for i in range(-5,5):
-    print(i)
     Weights.append([])
     for j in range(10):
         w1 = np.random.random_sample((D+1,M))-0.5+i
@@ -231,10 +233,11 @@ for i in range(-5,5):
         Weights[-1].append((w1,w2))
 
 
-for partWeight in Weights:
+for i, partWeight in enumerate(Weights):
+    print(i)
     ErrorW.append(0)
     for w1,w2 in partWeight:
-        NetworkTestW = NeuralNW(X,Y,eta)
+        NetworkTestW = NeuralNW(X,Y)
         NetworkTestW.setWeights(w1,w2)
         for i in range(100):
             NetworkTestW.TrainNetworkRand()
@@ -258,13 +261,20 @@ plt.ylabel('Error')
 
 
 Data = np.genfromtxt('data/a017_NNpdfGaussMix.txt')
-
 X2 = Data[:,0:2]
 Y2 = Data[:,2]
 Plot(X2,Y2,"2.5")
 
+#%%
 
+RealDataNNW = NeuralNW(X2,Y2,eta=0.01,M=80)
 
+for i in range(2000):
+    RealDataNNW.TrainNetworkRand()
+    if (i % 20 == 0):
+        RealDataNNW.PlotOutput("Real" + str(i),save=True,savename="../latex/Images/Final2/" + str(i) + ".png")
+        
+RealDataNNW.PlotOutput("Real" + str(i),save=True,savename="../latex/Images/Final2/" + str(i+1) + ".png")
 
 
 #%% This is to test the convergence for different Eta's in case it often overshoots --> Spoiler: that wasn't the case (Warning: Takes a long time to compute!)
